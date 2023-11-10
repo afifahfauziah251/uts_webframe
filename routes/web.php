ac366a836540f94e4b08c6fe66b0b771f138ad7b
afifahfauziah251 <afifahfauziah251@gmail.com>
2023-11-10 14:11:15 +0700

    jawaban soal 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('mahasiswa')->group(function () {
+    // Rute untuk halaman pendaftaran
+    Route::get('/pendaftaran', function () {
+        return 'Halaman Pendaftaran Mahasiswa';
+    })->name('mahasiswa.pendaftaran');
+
+    // Rute untuk halaman ujian
+    Route::get('/ujian', function () {
+        return 'Halaman Ujian Mahasiswa';
+    })->name('mahasiswa.ujian');
+
+    // Rute untuk halaman nilai
+    Route::get('/nilai', function () {
+        return 'Halaman Nilai Mahasiswa';
+    })->name('mahasiswa.nilai');
 });
+
+
